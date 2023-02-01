a9c6a5d90eaf8d5d2807cb3f5a3ab2ed216c03f6
Jahid Walid <jahidwalidme@gmail.com>
2023-02-02 01:34:06 +0600

    Course all item

--- src/components/CourseItem/CourseItem.js
+++ src/components/CourseItem/CourseItem.js
@@ -13,9 +13,16 @@ const CourseItem = () => {
                     <div className="course-item-box">
                     <img className='' src={data.image} alt="" />
                     <div className="course-item-box-bottom">
-                    <h6>{data.instructor}</h6>
+                    <div className="name-icon">
+                        <div className="name">
+                        <h6>{data.instructor}</h6>
+                        </div>
+                        <div className="icon">
+                        <i class="fa-sharp fa-solid fa-cart-shopping"></i> <i class="fa-solid fa-heart"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-share-nodes"></i>
+                        </div>
+                    </div>
                     <p className='course-title'>{data.title}</p>
-                    <p>Rating {data.rating}</p>
+                    <p><i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i>({data.rating})</p>
                     <div className="price-and-btn d-flex">
                     <h5>৳{data.price}</h5>
                     <button>BUY NOW</button>
